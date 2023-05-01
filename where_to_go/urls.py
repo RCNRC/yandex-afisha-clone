e41db57b20e6b81945f4ae85e72381ae73a72f05
Ilya <Dragoilya@yandex.ru>
2023-05-01 05:19:47 +0300

    Setup media files serving

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     #path('', views.index),
     path('', include('main_page.urls')),
     path('admin/', admin.site.urls),
-] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
