051fe9e734c6ac9047f940c6b4731ee9571737ed
Ilya <Dragoilya@yandex.ru>
2023-05-01 23:58:42 +0300

    Fixed problems

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -133,12 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-if DEBUG:
-    STATICFILES_DIRS = [
-        BASE_DIR / "static",
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
 ]
-else:
-    STATIC_ROOT = BASE_DIR / "static",
+
+STATIC_ROOT = BASE_DIR / "./"
 
 STATIC_URL = '/static/'
 
