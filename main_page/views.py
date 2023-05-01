0fab6f2e7342517a70f5b896b42b2ae7d1816bbf
Ilya <Dragoilya@yandex.ru>
2023-05-01 05:40:51 +0300

    Move data from template to view

--- main_page/views.py
+++ main_page/views.py
@@ -1,9 +1,45 @@
 from django.http import HttpResponse
 from django.template import loader
 
+def get_places():
+    places = {
+        "type": "FeatureCollection",
+        "features": [],
+    }
+    places["features"].append(
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.62, 55.793676]
+          },
+          "properties": {
+            "title": "«Легенды Москвы",
+            "placeId": "moscow_legends",
+            "detailsUrl": "static/places/moscow_legends.json"
+          }
+        }
+    )
+    places["features"].append(
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.64, 55.753676]
+          },
+          "properties": {
+            "title": "Крыши24.рф",
+            "placeId": "roofs24",
+            "detailsUrl": "static/places/roofs24.json"
+          }
+        }
+    )
+    return places
 
 def index(request):
     template = loader.get_template('index.html')
-    context = {}
+    context = {
+        "places": get_places()
+    }
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
