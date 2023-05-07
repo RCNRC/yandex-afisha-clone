ebdb8a6e5fac0e5264015379c73952c41a74f804
Ilya <Dragoilya@yandex.ru>
2023-05-08 00:30:48 +0300

    Removed redundant f in string

--- places/models.py
+++ places/models.py
@@ -1,18 +1,25 @@
 from django.db import models
 from tinymce.models import HTMLField
 
+
 class Place(models.Model):
     title = models.CharField(
         unique=True,
         max_length=201
     )
-    description_short = models.TextField()
-    description_long = HTMLField()
+    description_short = models.TextField(
+        null=True,
+        blank=True,
+    )
+    description_long = HTMLField(
+        null=True,
+        blank=True,
+    )
     lng = models.FloatField()
     lat = models.FloatField()
 
     def __str__(self):
-        return f"{self.title}"
+        return self.title
 
 
 class Image(models.Model):
